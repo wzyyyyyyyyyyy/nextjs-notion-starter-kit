f929341b9baa91747d7f919bb24c420e39d540a3
camboo <38101776+wzyyyyyyyyyyy@users.noreply.github.com>
2021-04-05 18:49:23 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -7,19 +7,19 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Quite A Boring Blog',
+  domain: 'blog.qilil.com',
+  author: 'Leo Weng',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Basic Talking About My Life And Some Bullshit',
+  socialImageTitle: 'I Love You!!!',
+  socialImageSubtitle: 'Hello This Fucking World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'Leoweng2021',
+  github: 'tmymy',
+  linkedin: 'nope',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
