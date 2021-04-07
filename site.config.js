d7c922b0dec763711ba82ca39cc9db09ca5d7c5a
camboo <38101776+wzyyyyyyyyyyy@users.noreply.github.com>
2021-04-07 10:56:18 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '858cfb78aeea40c0a4df225514be298e',
+  rootNotionPageId: 'HoliPort-858cfb78aeea40c0a4df225514be298e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
